ae457bb4dffc8e34f036dcc78c796b777d4586d2
Jonas <jonasong@berkeley.edu>
2024-06-11 16:52:49 -0700

    initial commit template

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <h1>Welcome to Jonas Portfolio</h1>
+        <p>This is my personal website</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
